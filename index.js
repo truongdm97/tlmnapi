e5ae1a1b0da38d3c9cbc07eaaa53ec8703b77bb6
truongdm97 <truongdm97@github.com>
2021-01-24 21:25:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -39,31 +39,98 @@ require('./api/routes/user.routes.js')(app, {
   }
 });
 
-// listen for requests
-// app.listen(3001);
-//app.listen(process.env.PORT || 3001);
-
 var server = require("http").Server(app);
 var io = require("socket.io")(server, {
   cors: {
     origin: '*'
   }
 });
-//server.listen(3002);
+
 server.listen(process.env.PORT || 80);
+
+var listJoined = [];
+var room1 = [];
+var room2 = [];
+var room3 = [];
+var room4 = [];
 
 io.on("connection", function(socket){
 
 	socket.on("disconnect", function(){
-		//
+		if (socket.Username) {
+			var user = socket.Username;
+	    	var room = socket.Userroom;
+
+			listJoined.splice(listJoined.indexOf(user), 1);
+	        switch(room) {
+				case "r1":
+					room1.splice(room1.indexOf(user), 1);
+					break;
+				case "r2":
+					room2.splice(room2.indexOf(user), 1);
+					break;
+				case "r3":
+					room3.splice(room3.indexOf(user), 1);
+					break;
+				case "r4":
+					room4.splice(room4.indexOf(user), 1);
+					break;
+			}
+		}
 	});
 
-	socket.on("client-send-username", function(data){
-		socket.Username = data;
-	});
+	socket.on("join", function(room, user) {
+		if (listJoined.includes(user)) {
+			socket.emit("server-send-user-joined");
+			return;
+		}
 
-	socket.on("user-send-messages", function(data){
-		socket.broadcast.emit("server-send-messages", {un:socket.Username, nd:data});
+        switch(room) {
+			case "r1":
+				if (room1.length > 3) {
+					socket.emit("server-send-room-full");
+					return;
+				} else {
+					socket.emit("server-send-joined-room", room1);
+					room1.push(user);
+				}
+				break;
+			case "r2":
+				if (room2.length > 3) {
+					socket.emit("server-send-room-full");
+					return;
+				} else {
+					socket.emit("server-send-joined-room", room2);
+					room2.push(user);
+				}
+				break;
+			case "r3":
+				if (room3.length > 3) {
+					socket.emit("server-send-room-full");
+					return;
+				} else {
+					socket.emit("server-send-joined-room", room3);
+					room3.push(user);
+				}
+				break;
+			case "r4":
+				if (room4.length > 3) {
+					socket.emit("server-send-room-full");
+					return;
+				} else {
+					socket.emit("server-send-joined-room", room4);
+					room4.push(user);
+				}
+				break;
+			}
+		listJoined.push(user);
+		socket.join(room);
+        socket.Userroom = room;
+        socket.Username = user;
+    });
+
+	socket.on("user-send-messages", function(room, data){
+		socket.broadcast.to(room).emit("server-send-messages", {un:socket.Username, nd:data});
 	});
 
 });
