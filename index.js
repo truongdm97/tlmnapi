d70b69697d54edd8f56965bfc7acf24dd13f3e47
truongdm97 <truongdm97@github.com>
2021-01-29 16:02:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -51,54 +51,60 @@ server.listen(process.env.PORT || 80);
 var listJoined = [];
 var room1 = [];
 var room2 = [];
-var room3 = [];
-var room4 = [];
 var room1Id = [];
 var room2Id = [];
-var room3Id = [];
-var room4Id = [];
 var numReadyRoom1 = 0;
 var numReadyRoom2 = 0;
-var numReadyRoom3 = 0;
-var numReadyRoom4 = 0;
 var room1Status = 0;
 var room2Status = 0;
-var room3Status = 0;
-var room4Status = 0;
 var listCard = ["c03a", "c04a", "c05a", "c06a", "c07a", "c08a", "c09a", "c10a", "c11a", "c12a", "c13a", "c14a", "c20a", "c03b", "c04b", "c05b", "c06b", "c07b", "c08b", "c09b", "c10b", "c11b", "c12b", "c13b", "c14b", "c20b", "c03c", "c04c", "c05c", "c06c", "c07c", "c08c", "c09c", "c10c", "c11c", "c12c", "c13c", "c14c", "c20c", "c03d", "c04d", "c05d", "c06d", "c07d", "c08d", "c09d", "c10d", "c11d", "c12d", "c13d", "c14d", "c20d"];
+var turnRoom1 = "";
+var turnRoom2 = "";
+var preWinRoom1 = "";
+var preWinRoom2 = "";
 
-function shuffleCard(room) {
+function shuffleCard(room, roomLength) {
 	var listCardShuffle = listCard;
-	var roomIdTemp, listCardUser = [];
+	var listCardPlay, listCardUser = [];
 	var i, j, k;
 	var startCard = 0;
 	var endCard = 13;
-	var tempCard = '';
+	var tempCard = "";
+	var minCard = "c03a";
 	for (i = 0; i < 52; i++) {
         j = Math.floor(Math.random() * 52);
         tempCard = listCardShuffle[i];
         listCardShuffle[i] = listCardShuffle[j];
         listCardShuffle[j] = tempCard;
     }
-    switch(room) {
+    listCardPlay = listCardShuffle.slice(0, roomLength * 13).sort();
+    minCard = listCardPlay[0];
+    
+	switch(room) {
     	case "r1":
-    		roomIdTemp = room1Id;
+	    	for (k = 0; k < roomLength; k++) {
+		    	listCardUser = listCardShuffle.slice(startCard, endCard).sort();
+		    	startCard = startCard + 13;
+		    	endCard = endCard + 13;
+		    	if (preWinRoom1 == "" && listCardUser.includes(minCard)) {
+		    		turnRoom1 = room1[k];
+		    	}
+		    	io.to(room1Id[k]).emit("server-send-card", listCardUser);
+		    }
+	    	io.in(room).emit("server-send-turn", turnRoom1);
     		break;
     	case "r2":
-    		roomIdTemp = room2Id;
+	    	for (k = 0; k < roomLength; k++) {
+		    	listCardUser = listCardShuffle.slice(startCard, endCard).sort();
+		    	startCard = startCard + 13;
+		    	endCard = endCard + 13;
+		    	if (preWinRoom2 == "" && listCardUser.includes(minCard)) {
+		    		turnRoom2 = room2[k];
+		    	}
+		    	io.to(room2Id[k]).emit("server-send-card", listCardUser);
+		    }
+	    	io.in(room).emit("server-send-turn", turnRoom2);
     		break;
-    	case "r3":
-    		roomIdTemp = room3Id;
-    		break;
-    	case "r4":
-    		roomIdTemp = room4Id;
-    		break;
-    }
-    for (k = 0; k < roomIdTemp.length; k++) {
-    	listCardUser = listCardShuffle.slice(startCard, endCard).sort();
-    	io.to(roomIdTemp[k]).emit("server-send-game-start", listCardUser);
-    	startCard = startCard + 13;
-    	endCard = endCard + 13;
     }
 }
 
@@ -132,35 +138,10 @@ io.on("connection", function(socket){
 					if (room2 == []) {
 						numReadyRoom2 = 0;
 					}
-					break;
-				case "r3":
-					room3.splice(room3.indexOf(user), 1);
-					room3Id.splice(room1Id.indexOf(socket.id), 1);
-					if (socket.Userready == 1) {
-						numReadyRoom3 = numReadyRoom3 - 1;
-					}
-					io.in(room).emit("server-send-joined-room", room3, numReadyRoom3);
-					if (room3 == []) {
-						numReadyRoom3 = 0;
-					}
-					break;
-				case "r4":
-					room4.splice(room4.indexOf(user), 1);
-					room4Id.splice(room1Id.indexOf(socket.id), 1);
-					if (socket.Userready == 1) {
-						numReadyRoom4 = numReadyRoom4 - 1;
-					}
-					io.in(room).emit("server-send-joined-room", room4, numReadyRoom4);
-					if (room4 == []) {
-						numReadyRoom4 = 0;
-					}
-					break;
-			}
+					break;			}
 			//tam thoi
 			room1Status = 0;
 			room2Status = 0;
-			room3Status = 0;
-			room4Status = 0;
 		}
 	});
 
@@ -193,39 +174,17 @@ io.on("connection", function(socket){
 					io.in(room).emit("server-send-joined-room", room2, numReadyRoom2);
 				}
 				break;
-			case "r3":
-				if (room3.length > 3) {
-					socket.emit("server-send-room-full");
-					return;
-				} else {
-					socket.join(room);
-					room3.push(user);
-					room3Id.push(socket.id);
-					io.in(room).emit("server-send-joined-room", room3, numReadyRoom3);
-				}
-				break;
-			case "r4":
-				if (room4.length > 3) {
-					socket.emit("server-send-room-full");
-					return;
-				} else {
-					socket.join(room);
-					room4.push(user);
-					room4Id.push(socket.id);
-					io.in(room).emit("server-send-joined-room", room4, numReadyRoom4);
-				}
-				break;
 			}
 		listJoined.push(user);
         socket.Userroom = room;
         socket.Username = user;
     });
 
-    socket.on("user-send-messages", function(data){
-		socket.broadcast.to(socket.Userroom).emit("server-send-messages", {un:socket.Username, nd:data});
+    socket.on("user-send-messages", function(message){
+		socket.broadcast.to(socket.Userroom).emit("server-send-messages", {un:socket.Username, ms:message});
 	});
 
-	socket.on("user-send-ready", function(data){
+	socket.on("user-send-ready", function(){
 		socket.Userready = 1;
 		var room = socket.Userroom
 		switch(room) {
@@ -238,7 +197,7 @@ io.on("connection", function(socket){
 				io.in(room).emit("server-send-joined-room", room1, numReadyRoom1);
 				if (numReadyRoom1 > 1 && numReadyRoom1 == room1.length) {
 					room1Status = 1;
-					shuffleCard(room);
+					shuffleCard(room, room1.length);
 				}
 				break;
 			case "r2":
@@ -250,37 +209,13 @@ io.on("connection", function(socket){
 				io.in(room).emit("server-send-joined-room", room2, numReadyRoom2);
 				if (numReadyRoom2 > 1 && numReadyRoom2 == room2.length) {
 					room2Status = 1;
-					shuffleCard(room);
-				}
-				break;
-			case "r3":
-				if (room3Status == 1) {
-					socket.emit("server-send-game-started");
-					return;
-				}
-				numReadyRoom1 = numReadyRoom1 + 1;
-				io.in(room).emit("server-send-joined-room", room3, numReadyRoom3);
-				if (numReadyRoom3 > 1 && numReadyRoom3 == room3.length) {
-					room3Status = 1;
-					shuffleCard(room);
-				}
-				break;
-			case "r4":
-				if (room4Status == 1) {
-					socket.emit("server-send-game-started");
-					return;
-				}
-				numReadyRoom1 = numReadyRoom1 + 1;
-				io.in(room).emit("server-send-joined-room", room4, numReadyRoom4);
-				if (numReadyRoom4 > 1 && numReadyRoom4 == room4.length) {
-					room4Status = 1;
-					shuffleCard(room);
+					shuffleCard(room, room2.length);
 				}
 				break;
 		}
 	});
 
-	socket.on("user-send-not-ready", function(data){
+	socket.on("user-send-not-ready", function(){
 		socket.Userready = 0;
 		var room = socket.Userroom
 		switch(room) {
@@ -292,13 +227,40 @@ io.on("connection", function(socket){
 				numReadyRoom2 = numReadyRoom2 - 1;
 				io.in(room).emit("server-send-joined-room", room2, numReadyRoom2);
 				break;
-			case "r3":
-				numReadyRoom3 = numReadyRoom3 - 1;
-				io.in(room).emit("server-send-joined-room", room3, numReadyRoom3);
+		}
+	});
+
+	socket.on("user-send-play", function(userSend, listCard){
+		var room = socket.Userroom;
+		var user = socket.Username;
+		var indexOfUser = 0;
+
+		switch(room) {
+			case "r1":
+				if (user == turnRoom1) {
+					indexOfUser = room1.indexOf(user);
+					indexOfUser = indexOfUser + 1;
+					if (indexOfUser == room1.length) {
+						indexOfUser = 0;
+					}
+					turnRoom1 = room1[indexOfUser];
+					io.in(room).emit("server-send-play-ok",userSend, listCard, turnRoom1);
+				} else {
+					socket.emit("server-send-play-error");
+				}
 				break;
-			case "r4":
-				numReadyRoom4 = numReadyRoom4 - 1;
-				io.in(room).emit("server-send-joined-room", room4, numReadyRoom4);
+			case "r2":
+				if (user == turnRoom2) {
+					indexOfUser = room2.indexOf(user);
+					indexOfUser = indexOfUser + 1;
+					if (indexOfUser == room2.length) {
+						indexOfUser = 0;
+					}
+					turnRoom2 = room2[indexOfUser];
+					io.in(room).emit("server-send-play-ok",userSend, listCard, turnRoom2);
+				} else {
+					socket.emit("server-send-play-error");
+				}
 				break;
 		}
 	});
